0491d10e10d61a8823279a61c2140aa6da40acbc
Fatkhurridlo <111202013093@mhs.dinus.ac.id>
2022-03-15 08:24:39 +0700

    Initial commit

---
+++ src/WhileLoop.kt
@@ -0,0 +1,11 @@
+// Program untuk membuat lima line
+
+fun main(args: Array<String>) {
+
+    var i = 1
+
+    while (i <= 5) {
+        println("Line $i")
+        ++i
+    }
+}
---
+++ src/range.kt
@@ -0,0 +1,8 @@
+fun main(args : Array<String>){
+
+    println("Integer range:")
+    // creating integer range
+    for(num in 1..5){
+        println(num)
+    }
+}
